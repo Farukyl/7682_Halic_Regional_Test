72b503d9d10a797e8af9fda043b52d06bab73a18
Farukyl <99757704+Farukyl@users.noreply.github.com>
2023-03-11 21:29:23 +0300

    Update TurretSubsystem.java

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -8,6 +8,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -16,8 +17,33 @@ public class TurretSubsystem extends SubsystemBase {
   CANSparkMax turret = new CANSparkMax(1, MotorType.kBrushless);
 
   MotorControllerGroup turreControllerGroup = new MotorControllerGroup(turret);
+
+  PIDController turretPidController = new PIDController(0.1, 0, 0);
+
+  double degreeSetPoint;
+
+  
+
+
+
+
+
+  
   /** Creates a new TurretSubsystem. */
-  public TurretSubsystem() {}
+  public TurretSubsystem() {
+  }
+
+  public double turretEncoderDegree() { 
+    double turretDegree = turret.getEncoder().getPosition()*360;
+    return turretDegree;
+  }
+
+  public void turretPID(double degreeSetPoint) {
+    double turretPIDcontrol = turretPidController.calculate(turretEncoderDegree(), degreeSetPoint);
+    turreControllerGroup.set(turretPIDcontrol);
+  
+  }
+
 
   public void turret(double speed){
     turreControllerGroup.set(speed);
@@ -27,6 +53,6 @@ public class TurretSubsystem extends SubsystemBase {
     }
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+      // This method will be called once per scheduler run
   }
 }
