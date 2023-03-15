d330438f577dca277514117469dfe2af0131fe70
Farukyl <99757704+Farukyl@users.noreply.github.com>
2023-03-15 22:49:24 +0300

    Update ClimbSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -16,8 +17,26 @@ public class ClimbSubsystem extends SubsystemBase {
   CANSparkMax climbMax2 = new CANSparkMax(3, MotorType.kBrushless);
 
   MotorControllerGroup climbControllerGroup = new MotorControllerGroup(climbMax1, climbMax2);
+
+  PIDController climbPidController = new PIDController(0.1, 0, 0);
+
+  double centimeterSetPoint;
   /** Creates a new Climb. */
   public ClimbSubsystem() {}
+
+  public void climbEncoderReset() {
+    climbMax1.getEncoder().setPosition(0);  
+  }
+
+  public double climbEncoderCentimeter(){
+    double climbCentimeter = climbMax1.getEncoder().getPosition()*150;
+    return climbCentimeter;
+  }
+
+  public void climbPID(double centimeterSetPoint){
+    double climbPidControl = climbPidController.calculate(climbEncoderCentimeter(),centimeterSetPoint);
+    climbMax1.set(climbPidControl);
+  }
 
   public void climbIn(double speed) {
     climbControllerGroup.set(speed);
